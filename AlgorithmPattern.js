2132ac00f5e783c8ee343f59aabe3696eb9d6f5e
gywgithub <qingyi_w@outlook.com>
2020-09-29 12:29:23 +0800

    add lcs

--- AlgorithmPattern.js
+++ AlgorithmPattern.js
@@ -145,3 +145,66 @@ function findValues(n, capacity, kS, weights, values) {
     }
   }
 }
+
+console.log('---')
+
+/**
+ * 最长公共子序列（LCS）：找出两个字符串序列的最长子序列的长度。最长子序列是指，在两个字符串序列中以相同顺序出现，但不要求连续（非字符串子串）的字符串序列。
+ */
+function lcs(wordX, wordY) {
+  var m = wordX.length,
+  n = wordY.length,
+  l = [],
+  i, j, a, b;
+  // var solution = [];
+
+  for (i = 0; i <= m; ++i) {
+    l[i] = [];
+    // solution[i] = [];
+    for (j = 0; j <= n; ++j) {
+      l[i][j] = 0;
+      // solution[i][j] = 0;
+    }
+  }
+
+  for (i = 0; i <= m; i++) {
+    for (j = 0; j <= n; j++) {
+      if (i == 0 || j == 0) {
+        l[i][j] = 0;
+      } else if (wordX[i - 1] == wordY[j - 1]) {
+        l[i][j] = l[i - 1][j - 1] + 1;
+        // solution[i][j] = 'diagonal';
+      } else {
+        a = l[i - 1][j];
+        b = l[i][j - 1];
+        l[i][j] = (a > b) ? a : b; // max(a, b)
+        // solution[i][j] = (l[i][j] == l[i - 1][j]) ? 'top' : 'left';
+      }
+    }
+  }
+  // printSolution(solution, l, wordX, wordY, m, n);
+  return l[m][n];
+}
+
+function printSolution(solution, l, wordX, wordY, m, n) {
+  var a = m, b = n, i, j,
+  x = solution[a][b],
+  answer = '';
+
+  while (x !== '0') {
+    if (solution[a][b] === 'diagonal') {
+      answer = wordX[a - 1] + answer;
+      a--;
+      b--;
+    } else if (solution[a][b] === 'left') {
+      b--;
+    } else if (solution[a][b] === 'top') {
+      a--;
+    }
+    x = solution[a][b];
+  }
+  console.log('lcs: ' + answer);
+}
+
+let resLcs = lcs('acbaed', 'abcadf')
+console.log('res: ', resLcs)
